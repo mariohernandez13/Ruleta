e51b52c6ff27cb0136d176b0e93f6b143431bfad
Mario Hernández <150152618+mariohernandez13@users.noreply.github.com>
2024-07-16 17:07:46 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ const botonGirar = document.getElementById('girar');
 const resultado = document.getElementById('resultado');
 
 const segmentos = [
-    { premio: 'Premio 1', probabilidad: 0.1 },
-    { premio: 'Premio 2', probabilidad: 0.2 },
-    { premio: 'Premio 3', probabilidad: 0.25 },
+    { premio: 'Tira otra vez', probabilidad: 0.3 },
+    { premio: 'Reto', probabilidad: 0.1 },
+    { premio: 'Beso', probabilidad: 0.5 },
     { premio: 'Premio 4', probabilidad: 0.15 },
     { premio: 'Premio 5', probabilidad: 0.2 },
-    { premio: 'Premio 6', probabilidad: 0.1 }
+    { premio: 'Copa Gratis', probabilidad: 0 }
 ];
 const numSegmentos = segmentos.length;
 const anguloSegmento = 360 / numSegmentos;
